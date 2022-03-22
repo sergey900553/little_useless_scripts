548611a192d2eb868140493d6d9c3ec6239e8e4d
sergey900553 <42143402+sergey900553@users.noreply.github.com>
2022-03-22 18:00:47 +0300

    Update select_links.js

--- select_links.js
+++ select_links.js
@@ -1,3 +1,21 @@
+/* проще
 let elements = document.querySelectorAll(`.video-thumb-info a`);
 let output = Array.from(elements).map(i=>{return i.href}).join("<br>");
 window.open().document.write(output);
+*/
+
+
+function urlSiteCrop(url) {
+    let output = url.replace(/^/, `"`).replace(/$/, `",`);
+    return output;
+};
+
+let elements = document.querySelectorAll(`.video-thumb-info a`);
+let output = Array.from(elements).map(i => { return i.href });
+
+let govno = [];
+for (let i = 0; i < output.length; i++) {
+    govno[i] = urlSiteCrop(output[i]) + "" + ("<br>");
+}
+
+window.open().document.write(govno.join(""));
