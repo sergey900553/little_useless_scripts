86b88f8f95f0b063ac73c919ce0ff03c8864ce0a
sergey900553 <42143402+sergey900553@users.noreply.github.com>
2022-05-17 08:33:56 +0300

    Create mixCases.js

---
+++ mixCases.js
@@ -0,0 +1,21 @@
+function mixCases(text) {
+    let output = '';
+
+    for (let i = 0; i < text.length; i++) {
+        let generateOneOrZero = Math.floor(Math.random() * 2);
+
+        if (i == 0) {
+            output = output + text[i].toUpperCase();
+            continue;
+        }
+        if (generateOneOrZero == 0) {
+            output = output + text[i].toUpperCase();
+        }
+        else if (generateOneOrZero == 1) {
+            output = output + text[i].toLowerCase();
+        }
+    }
+    return output;
+}
+
+console.log(mixCases("Даже, если Симоньян подсыпит новичка кому надо, то ее это не спасет"));
