8e0dea24c8ba5670fb6c4ba1d1e15107ce44e7ef
sergey900553 <42143402+sergey900553@users.noreply.github.com>
2022-11-02 16:24:26 +0300

    Update openfiles.py

--- openfiles.py
+++ openfiles.py
@@ -1,18 +1,11 @@
 import os
-dir = r'C:\Users\serge\Downloads\s'
-for file in os.listdir(dir):
-    if file.endswith(".osk"):
-        os.startfile(os.path.join(dir, file))
-        print(os.path.join(dir, file))
-
-        
-import os
 import glob
 dir = r'C:\Users\serge\Downloads\s\*.osk'
 files = glob.glob(dir)
 
 for file in files:
     if file.endswith(".osk"):
+        print(file)
         os.startfile(file)
 
      
